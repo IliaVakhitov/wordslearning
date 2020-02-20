4dc69552e674595e8bde6e9f317c8bd4167d63df
Ilia Vakhitov <iv7180@gmail.com>
2020-02-20 14:21:59 +1000

    Added popover to display error messages

--- app/static/define_game.js
+++ app/static/define_game.js
@@ -1,1 +1,17 @@
 $('select_dictionaries').selectpicker();
+
+$(function () {
+    $("#game_rounds")
+        .popover({ content: "Please, select value more than 3!" })
+        .blur(function () {
+            $(this).popover('hide');
+        });
+});
+
+function check_game_rounds() {
+    current_value = document.getElementById('game_rounds').value;
+    if (current_value < 4){
+        document.getElementById('game_rounds').value = 4;
+    }
+};
+
--- app/static/dictionaries.js
+++ app/static/dictionaries.js
@@ -5,20 +5,22 @@ function showFunction() {
     if (edit_block.style.display === "none") {
         edit_block.style.display = "block";
         btn.style.display = "none";
-        document.getElementById('err_message').style.display = "none";
     } else {
         edit_block.style.display = "none";
         btn.style.display = "inline";
     }
 }
 
+function hide_popover(){
+    $('#dictionary_name').popover('hide');
+}
 function check_new_dictionary_name() {
     $.post('/check_dictionary_name', {dictionary_name: document.getElementById('dictionary_name').value}
     ).done(function(response) {
         if (response.name_available) {
              document.getElementById("new_dictionary").submit();
         } else {
-            document.getElementById('err_message').style.display = "block";
+            $('#dictionary_name').popover('show');
         }
     }).fail(function() {
         //
